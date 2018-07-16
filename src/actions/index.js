78fc09c170fa34e71e409d7dbb82dca8fa168c4d
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-07-17 00:05:08 +0500

    adding user login success action/reducer/type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firbase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -13,7 +13,7 @@ export const loginUser = ({ email, password }) => {
     firbase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
         console.log(user);
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
       });
   }
 }
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,9 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 /**
@@ -22,6 +23,11 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         password: action.payload
+      };
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload
       };
     default: return state;
   }
