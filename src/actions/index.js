678e400506debbfd8b7e838948c9cc8088dade46
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-07-17 00:15:59 +0500

    adding user login failed action/reducer/type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 import firbase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FILED
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -8,19 +13,29 @@ export const emailChanged = (text) => {
   };
 }
 
-export const loginUser = ({ email, password }) => {
-  return (dispatch) => {
-    firbase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        console.log(user);
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-      });
-  }
-}
-
 export const passwordChanged = (text) => {
   return {
     payload: text,
     type: PASSWORD_CHANGED
   };
 };
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firbase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        console.log(user);
+        loginUserSuccess(dispatch, user);
+      })
+      .catch(() => {
+        firbase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(()=>loginUserFailed(dispatch));
+      });
+  }
+}
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+}
+const loginUserFailed = dispatch => dispatch({ type: LOGIN_USER_FILED });
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password _changed';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FILED = 'login_user_filed';
